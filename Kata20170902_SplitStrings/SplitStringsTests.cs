255b9faddfdaf11b44a79ae72f1737f35bc0c2c9
cash <cash@cashwu.com>
2017-09-02 17:21:07 +0800

    init

--- Kata20170902_SplitStrings/SplitStringsTests.cs
+++ Kata20170902_SplitStrings/SplitStringsTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170902_SplitStrings
 {
     [TestClass]
-    public class UnitTest1
+    public class SplitStringsTests
     {
         [TestMethod]
         public void TestMethod1()
